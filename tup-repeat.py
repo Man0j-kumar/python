87bf843c027bce73a3ab6c14316280ccc25c8420
Man0j-kumar <53567730+Man0j-kumar@users.noreply.github.com>
2019-08-11 13:20:30 +0530

    Update tup-repeat.py

--- tup-repeat.py
+++ tup-repeat.py
@@ -1,6 +1,8 @@
 #Python program to find the repeated items of a tuple
-a=input().split(",")
+a=input().split()
 b=tuple(a)
-print(b)
-count=b.count('4')
-print(count)
+s=set(b)
+for i in s:
+    c=b.count(i)
+    if(c>1):
+        print(i)
