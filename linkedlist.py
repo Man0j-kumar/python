43e9e8ce2b90c2b8f9318ea4724d23786ac46629
Man0j-kumar <53567730+Man0j-kumar@users.noreply.github.com>
2019-08-16 15:56:06 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -5,32 +5,85 @@ class Node:
 class SLL:
     def __init__(self):
         self.head=None
-    def insertatbeg(self,d):
+    def insert_beg(self,d):
         temp=Node(d)
         temp.nxt=self.head
         self.head=temp
         self.printval()
         print("\n")
-    def deleteatbeg(self):
+    def insert_mid(self,d,position):
+        newnode=Node(d)
+        temp=self.head
+        pos=0
+        while((pos+1)<position):
+            temp=temp.nxt
+            if(temp==None):
+                print("Invalid position")
+                return
+            pos=pos+1
+        newnode.nxt=temp.nxt
+        temp.nxt=newnode
+        self.printval()
+        print("\n")
+    def insert_end(self,d):
+        newnode=Node(d)
+        temp=self.head
+        while(temp.nxt!=None):
+            temp=temp.nxt
+        temp.nxt=newnode
+        self.printval()
+        print("\n")
+    def delete_beg(self):
         temp=self.head
         self.head=self.head.nxt
         temp.nxt=None
         self.printval()
         print("\n")
+    def delete_mid(self,position):
+        pos=0
+        temp=self.head
+        while((pos+1)<position):
+            temp=temp.nxt
+            if(temp==None):
+                print("Invalid position")
+                return
+            pos=pos+1
+        temp1=temp.nxt
+        temp.nxt=temp1.nxt
+        self.printval()
+    def delete_end(self):
+        temp=self.head
+        while(temp.nxt.nxt!=None):
+            temp=temp.nxt
+        temp.nxt=None
+        self.printval()
     def printval(self):
         temp=self.head
-        while(temp):
+        while(temp.nxt!=None):
             print(temp.data,"==>",end="")
             temp=temp.nxt
+        print(temp.data)
 ll=SLL()
 ch=0
-while ch!=4:
-    print(" 1.Insert a node at begining \n 2.Delete the head node \n 3.Print the linked list \n 4.Exit \n")
-    ch=int(input())
+while ch!=8:
+    print("1.Insert a node at begining \n2.Insert at the middle \n3.Insert at the end \n4.Delete the head node \n5.Delete at the middle \n6.Delete at the end \n7.Print the linked list \n8.Exit")
+    ch=int(input("Enter your choice:"))
     if(ch==1):
-        data=input("Enter the data: ")
-        ll.insertatbeg(data)
+        data=input("Enter the data:")
+        ll.insert_beg(data)
     elif(ch==2):
-        ll.deleteatbeg()
+        data=input("Enter the data:")
+        position=int(input("Enter the position:"))
+        ll.insert_mid(data,position)
     elif(ch==3):
+        data=input("Enter the data:")
+        ll.insert_end(data)
+    elif(ch==4):
+        ll.delete_beg(data,position)
+    elif(ch==5):
+        position=int(input("Enter the position:"))
+        ll.delete_mid(position)
+    elif(ch==6):
+        ll.delete_end()
+    elif(ch==7):
         ll.printval()
